3b4e00f9e0f5080d8addc821fd957aefbcc3ce8e
102 - Practica Libre <102@laboratorios.usbbog.edu.co>
2023-03-09 12:55:28 -0500

    prueba de archivo

---
+++ factorial_1.py
@@ -0,0 +1,7 @@
+from factorial import fcn_factorial as f 
+
+def main():
+    n=int(input('Ingrese el numero de grupos:'))
+    m=int(input('Ingrese el numero de muestra:'))
+    result =((f(n))/(f(m))*f(n-m))
+    print(f'El numero de convinaciones posibles es:'{result})
