e37b286a3e03005f285702f137622cceecdb044a
HelenLC <125518002+HelenLC@users.noreply.github.com>
2023-03-09 15:05:03 -0500

    Add files via upload

---
+++ Parcial1.py
@@ -0,0 +1,11 @@
+n=int(input("Ingrese un numero :"))
+if n<=7:
+    print('Salto')
+    if a ==7: 
+        a=iguales
+         return a
+          print(f"Digitos iguales a 7:",{a})
+          if b ==7:
+            b=diferentes
+            return b
+            print(f"Digitos diferentes a 7",{b})
